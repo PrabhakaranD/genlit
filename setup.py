7f093be32dd6fbc085161a488c714f63e5eaa395
Prabhakaran <get2jawa@gmail.com>
2024-03-28 23:45:09 +0800

    structure created

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='genlit',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask'
+    ],
+    entry_points={
+        'console_scripts': [
+            'genlit=genlit.app:main',  # This line assumes you have a main function in app.py that serves as the entry point to your application
+        ],
+    },
+)
